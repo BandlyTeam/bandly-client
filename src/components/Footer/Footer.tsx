c565d5f5f89309be31a5d8625c75954b7f73b378
nikkahl <galtveronika1@gmail.com>
2025-04-15 20:25:12 +0300

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,28 @@
 import s from "./Footer.module.scss";
+import { FaFacebookF, FaInstagram, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { IoArrowForward } from "react-icons/io5";
 
 export default function Footer() {
   return (
     <div className={s.footer}>
       <div className={s.footerContent}>
-        <p className={s.footerText}>© 2025 Denfsa. All rights reserved.</p>
+        <div className={s.subscribeSection}>
+          <p className={s.title}>SUBSCRIBE TO OUR EMAILS</p>
+          <div className={s.inputWrapper}>
+            <input type="email" placeholder="EMAIL" />
+            <button>
+              <IoArrowForward />
+            </button>
+          </div>
+        </div>
+        <div className={s.socials}>
+          <FaFacebookF />
+          <FaInstagram />
+          <FaXTwitter />
+          <FaYoutube />
+        </div>
       </div>
+      <p className={s.footerText}>© 2025 BANDY TEAM. All rights reserved.</p>
     </div>
   );
 }
